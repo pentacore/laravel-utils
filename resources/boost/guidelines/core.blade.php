47afcd793065244f99439e6e86a33b207a812ae0
Martin Claesson <contact@pentacore.se>
2026-05-07 12:39:46 +0200

    fix: publish laravel/boost guidelines

---
+++ resources/boost/guidelines/core.blade.php
@@ -0,0 +1,10 @@
+@php
+    /** @var \Laravel\Boost\Install\GuidelineAssist $assist */
+@endphp
+# Laravel Utils
+
+This project uses `pentacore/laravel-utils` to import some basic utilities, traits and helpers.
+
+## Concerns/Traits
+- `@php \Pentacore\LaravelUtils\Concerns\EnumUtils::class @endphp` - A trait for enums that provides some utility methods to get values and to pass values to the frontend.
+
